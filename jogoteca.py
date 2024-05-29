d4f063832eb7ff2e70989b96c14c1da7c2de0420
Tiago <tiagojss.santos@gmail.com>
2024-05-28 22:44:54 -0300

    Implementando Template html para tornar o html dinamico utilizando o motor de templates jinja2 que é integrado ao flask

---
+++ jogoteca.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from models.jogo import Jogo
+
+jogo1 = Jogo('Batman Knight', 'Aventura', 'PS4/PC')
+jogo2 = Jogo('Assassin Creed', 'Aventura', 'PS4/PC')
+jogo3 = Jogo('Tomb Raider', 'Aventura', 'PS4/PC/XBOX')
+jogo4 = Jogo('FIFA 24', 'Esporte', 'PS4/PC/XBOX')
+
+game_list = [jogo1, jogo2, jogo3, jogo4]
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_home():
+    return render_template('lista.html', title='Biblioteca de Jogos', games=game_list)     
+
+@app.route('/newgame')
+def new_game():
+    return render_template('novo_jogo.html', title='Novo Jogo')
+
+@app.route('/create', methods=['POST',])
+def create_game():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    new_game = Jogo(nome, categoria, console)
+    game_list.append(new_game)
+    return redirect('/')
+
+app.run(debug=True)
